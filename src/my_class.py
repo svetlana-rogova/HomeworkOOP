6da6c76f21932dd099f5dc29453ea714172e2aa6
Светлана Рогова <mifistofiya@gmail.com>
2025-11-08 21:09:24 +0300

    add classmethods, property, setter and access mode

--- src/my_class.py
+++ src/my_class.py
@@ -1,5 +1,6 @@
 class Product:
     """Класс для представления продуктов"""
+
     name: str
     description: str
     price: float
@@ -8,12 +9,44 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, dict_product, category):
+        for product in category._Category__products:
+            if product.name == dict_product["name"]:
+                product.quantity += dict_product["quantity"]
+            if dict_product["price"] > product.price:
+                product.price = dict_product["price"]
+                return product
+
+        return cls(
+            name=dict_product["name"],
+            description=dict_product["description"],
+            price=dict_product["price"],
+            quantity=dict_product["quantity"],
+        )
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+        elif self.__price > new_price:
+            answer = input("Вы действительно хотите уменьшить цену товара?\nВведите 'y' если да.")
+            if answer != "y":
+                return
+        self.__price = new_price
 
 
 class Category:
     """Класс для представления категорий"""
+
     name: str
     description: str
     products: list
@@ -24,6 +57,17 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+    @property
+    def products(self):
+        product_list = ""
+        for el in self.__products:
+            product_list += f"{el.name}, {el.price} руб. Остаток: {el.quantity} шт.\n"
+        return product_list
+
+    def add_product(self, products: Product):
+        self.__products.append(products)
+        Category.product_count += 1
